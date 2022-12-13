ed06bb66bb14380d8dce976e4357bd27b475e494
itsds1124@gmail.com <itsds124@gmail.com>
2022-12-13 19:06:15 +0900

    'bulid'

---
+++ src/main/java/com/example/UserDto.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToSting;
+
+@Getter
+@Setter
+@ToString
+
+
+
+
+
+public class UserDto {
+    @Override
+    public String toString() {
+
+    }
+}
